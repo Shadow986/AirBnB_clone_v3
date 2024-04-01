d62f318839c3327a57eef19d2e32043e404a6c8b
Ben Vilakazi <xolanithegeengubane@gmail.com>
2024-04-01 09:50:35 +0200

    app

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,30 +1,46 @@
 #!/usr/bin/python3
-"""
-This is the 'app' module.
-
-This module provides a Flask application instance.
-"""
-
+"""Endpoint (route) will be to return the status of your API"""
+import os
 from flask import Flask
 from models import storage
 from api.v1.views import app_views
-import os
+from flask_cors import CORS
 
-# Create a variable app, instance of Flask
+
+# creating a Flask app
 app = Flask(__name__)
+CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
 
-# Register the blueprint app_views to your Flask instance app
-app.register_blueprint(app_views)
+
+app.register_blueprint(app_views, url_prefix="/api/v1")
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return {"error": "Not found"}, 404
+
+
+@app.errorhandler(400)
+def page_not_found(e):
+    message = e.description
+    return message, 400
+
 
 @app.teardown_appcontext
-def close_db(error):
-    """
-    Closes the storage on teardown
-    """
+def close(ctx):
     storage.close()
 
+
+if os.getenv("HBNB_API_HOST"):
+    host = os.getenv("HBNB_API_HOST")
+else:
+    host = "0.0.0.0"
+
+if os.getenv("HBNB_API_PORT"):
+    port = int(os.getenv("HBNB_API_PORT"))
+else:
+    port = 5000
+
+
 if __name__ == "__main__":
-    # Run your Flask server (variable app) with:
-    host = os.getenv('HBNB_API_HOST', '0.0.0.0')
-    port = os.getenv('HBNB_API_PORT', '5000')
     app.run(host=host, port=port, threaded=True)
