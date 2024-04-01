2a5cd6bd47a9fe10da019ec1770577261597a733
Shadow986 <archie986jr@gmail.com>
2024-04-01 04:44:16 -0700

    yes

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -7,6 +7,7 @@ This module provides a Flask Blueprint instance with a '/api/v1' prefix.
 
 from flask import Blueprint
 from api.v1.views.index import *
+from api.v1.views.state import *
 
 # Create a variable app_views which is an instance of Blueprint
 app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
