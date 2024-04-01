ea43632e2caa93d1a23868f74d82070ce36bf749
Shadow986 <91501629+Shadow986@users.noreply.github.com>
2024-04-01 08:31:38 +0200

    Update file_storage.py

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -67,13 +67,3 @@ class FileStorage:
         """Deserialize JSON file to objects"""
         self.__objects.clear()
         self.reload()
-
-    def get(self, cls, id):
-        """Returns obj based on cls and id else None"""
-        return self.__objects.get(cls + '.' + id, None) \
-            if type(cls) == str and type(id) == str else None
-
-    def count(self, cls=None):
-        """Count number of objects in storage or specific number
-        of cls objects"""
-        return len(self.all(cls))
